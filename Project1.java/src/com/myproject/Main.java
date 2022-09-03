a9d37cadbacd5601f6bbf6f09f80deeaa1ca939c
Youngkwan-Cho <dudrhks1009@naver.com>
2022-09-03 15:40:52 +0900

    Add Java files

--- Project1.java/src/com/myproject/Main.java
+++ Project1.java/src/com/myproject/Main.java
@@ -2,12 +2,13 @@ package com.myproject;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		Main m= new Main();
+		m.run(args);
 	}
 
-	
-	
-	
-	
+	public void run(String[] args) {
+		WordManager s = new WordManager();
+		s.start();		
+	}
 	
 }
